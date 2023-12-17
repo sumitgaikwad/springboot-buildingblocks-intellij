2d5d0fda65ca50cc5db1c017b9c3849e15803ad1
sumitgaikwad <gaikwad.sumit.4s@gmail.com>
2023-12-17 14:43:38 +0530

    First commit

---
+++ src/main/java/com/devpracticals/restservices/SpringbootBuildingblocksIntellijApplication.java
@@ -0,0 +1,13 @@
+package com.devpracticals.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblocksIntellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblocksIntellijApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpracticals/restservices/SpringbootBuildingblocksIntellijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpracticals.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingblocksIntellijApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
